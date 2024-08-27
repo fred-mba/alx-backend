fca3af6d474bce5ec908bef7ce1cd86b2bb5a051
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-08-27 16:27:01 +0300

    Node Redis Client

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,9 +1,10 @@
-import { createClient } from "redis";
+import { createClient } from 'redis';
 
 /**
  * Should connect to the Redis server running on your machine:
  * `Redis client connected to the server` when the connection to Redis works correctly
- * `Redis client not connected to the server: ERROR_MESSAGE` when the connection to Redis does not work
+ * `Redis client not connected to the server: ERROR_MESSAGE` when the connection to
+ * Redis does not work
  */
 const client = createClient();
 
