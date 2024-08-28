f5f257abd124b076977f8ac45f87c4e33b3198de
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-08-28 13:42:19 +0300

    Create the Job processor

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -7,7 +7,6 @@ const sendNotification = (phoneNumber, message) => {
 };
 queue.process('push_notification_code', (job, done) => {
   const { phoneNumber, message } = job.data;
-
   sendNotification(phoneNumber, message);
   done();
 });
